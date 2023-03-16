df6d7994351070bb9ad0e4f7eb62fabd4d5c8080
lungeloncube <93097279+lungeloncube@users.noreply.github.com>
2023-03-16 14:06:30 +0200

    access token refresh

--- api/auth/views.py
+++ api/auth/views.py
@@ -3,7 +3,7 @@ from flask import request
 from ..models.users import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from http import HTTPStatus
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, get_jwt_identity)
 
 auth_namespace = Namespace('auth', description='a namespace for authentication')
 
@@ -70,3 +70,13 @@ class Login(Resource):
                 'refresh_token': refresh_token
             }
             return response, HTTPStatus.OK
+
+
+@auth_namespace.route('/refresh')
+class Refresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        email = get_jwt_identity()
+        access_token = create_access_token(identity=email)
+
+        return {'access_token': access_token}, HTTPStatus.OK
