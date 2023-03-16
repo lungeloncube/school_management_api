82273020be3e3dc78d89938ac638cb95bea23c25
lungeloncube <93097279+lungeloncube@users.noreply.github.com>
2023-03-16 10:59:47 +0200

    added signup endpoint

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,7 +1,31 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
+from flask import request
+from ..models.users import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from http import HTTPStatus
 
 auth_namespace = Namespace('auth', description='a namespace for authentication')
 
+signup_model = auth_namespace.model(
+    'User', {
+        'id': fields.Integer(),
+        'name': fields.String(required=True, description="User`s name"),
+        'email': fields.String(required=True, description="User`s email address"),
+        'password': fields.String(required=True, description="The users password")
+    }
+)
+user_model = auth_namespace.model(
+    'User',{
+        'id': fields.Integer(),
+        'name': fields.String(required=True, description="User`s name"),
+        'email': fields.String(required=True, description="User`s email address"),
+        'password_hash': fields.String(required=True, description="The users password"),
+        'is_active':fields.Boolean(description="Shows if user is active"),
+        'is_staff': fields.Boolean(description="Show if user is staff"),
+        'is_admin': fields.Boolean(description="shows sif user is admin")
+
+    }
+)
 
 @auth_namespace.route('/')
 class HelloAuthentication(Resource):
@@ -11,9 +35,20 @@ class HelloAuthentication(Resource):
 
 @auth_namespace.route('/signup')
 class SignUp(Resource):
+
+
+    @auth_namespace.expect(signup_model)
+    @auth_namespace.marshal_with(user_model)
     def post(self):
         """create user"""
-        pass
+        data = request.get_json()
+        new_user = User(
+            name=data.get('name'),
+            email=data.get('email'),
+            password_hash=generate_password_hash(data.get('password'))
+        )
+        new_user.save()
+        return new_user, HTTPStatus.CREATED
 
 
 @auth_namespace.route('login')
--- api/models/users.py
+++ api/models/users.py
@@ -6,10 +6,17 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(), nullable=False, unique=True)
-    password_harsh = db.Column(db.Text(), nullable=False)
+    password_hash = db.Column(db.Text(), nullable=False)
     is_staff = db.Column(db.Boolean(), default=False)
     is_admin = db.Column(db.Boolean(), default=False)
     is_active = db.Column(db.Boolean(), default=False)
     courses = db.relationship('Course', backref='users', lazy=True)
     def __repr__(self):
         return f"<User {self.name}>"
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
